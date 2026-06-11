5df53acb0280d40d5efb7a22475b37182771ce5f
sukanjujumuriku-rgb <sukanjujumuriku@gmail.com>
2026-06-11 15:10:38 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,6 +2,10 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebas
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD26YA6F6g0ln9o53hyTUkDYglmaa91RG4",
   authDomain: "school-quiz-maker-60a6b.firebaseapp.com",
@@ -14,3 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const db =
+    getFirestore(app);
